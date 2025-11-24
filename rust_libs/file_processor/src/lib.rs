a50b6cc576c0708b3ba031039069708ef3482254
justint-rl <justint1004@gmail.com>
2025-11-24 00:12:20 -0500

    Support reading multiline

--- rust_libs/file_processor/src/lib.rs
+++ rust_libs/file_processor/src/lib.rs
@@ -1,5 +1,30 @@
+use std::fs;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
+}
+
+/**
+* Load content of file as raw string
+*/
+pub fn load_raw(file_path: &str) -> String {
+    let content = fs::read_to_string(file_path)
+      .expect(&format!("Could not load file {}", file_path));
+    content
+}
+
+pub fn load_multiline(file_path: &str) -> io::Result<Vec<String>> {
+    let file = File::open(file_path)?;
+    let reader = BufReader::new(file);
+    let mut lines = Vec::new();
+
+    for line in reader.lines() {
+        lines.push(line?);
+    }
+
+    Ok(lines)
 }
 
 #[cfg(test)]
