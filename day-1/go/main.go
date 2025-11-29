49c51356564d9c387e9438959000534cee0ebb1b
justint-rl <justint1004@gmail.com>
2025-11-29 12:49:54 -0500

    Integrate part 1 and 2 to main.go

--- day-1/go/main.go
+++ day-1/go/main.go
@@ -3,11 +3,20 @@ package main
 import (
 	"fmt"
 
+	notquitelisp "github.com/justint-rl/advent-of-code-2015/day-1/not_quite_lisp"
 	fileprocessor "github.com/justint-rl/advent-of-code-2015/go_libs/file_processor"
 )
 
 func main() {
 	fp := fileprocessor.New()
 	data := fp.LoadRaw("day-1/go/input/input.txt")
-	fmt.Println(data)
+	nql := notquitelisp.New()
+
+	// Part 1
+	result1 := nql.Part1(data)
+	fmt.Printf("Part 1: %d\n", result1)
+
+	// Part 2
+	result2 := nql.Part2(data)
+	fmt.Printf("Part 2: %d\n", result2)
 }
--- day-1/go/solution/not_quite_lisp.go
+++ day-1/go/solution/not_quite_lisp.go
@@ -1,1 +1,20 @@
 package not_quite_lisp
+
+type NotQuiteLisp interface {
+	Part1(data string) int64
+	Part2(data string) int64
+}
+
+type notQuiteLisp struct{}
+
+func New() NotQuiteLisp {
+	return &notQuiteLisp{}
+}
+
+func (n *notQuiteLisp) Part1(data string) int64 {
+	return 0
+}
+
+func (n *notQuiteLisp) Part2(data string) int64 {
+	return 0
+}
